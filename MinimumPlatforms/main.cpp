aeb99fd6fb3babeaa80530a908a625729b735a10
Abraham Kim <iukjgo@gmail.com>
2018-11-27 23:02:11 -0500

    MinumimPlatforms - try 1

--- MinimumPlatforms/main.cpp
+++ MinimumPlatforms/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
+
+bool compare(pair<int, int> left, pair<int, int> right) {
+   return left.second < right.second;
+}
 
 int main() {
    int T;
@@ -10,19 +16,35 @@ int main() {
    while (T--) {
       int N;
       cin >> N;
-      vector<int> arrivals(N);
-      vector<int> departures(N);
+      vector<pair<int,int>> times(N);
 
       for (int i = 0; i < N; i++) {
-         cin >> arrivals[i];
+         cin >> times[i].first;
       }
 
       for (int i = 0; i < N; i++) {
-         cin >> departures[i];
+         cin >> times[i].second;
       }
 
+      sort(times.begin(), times.end(), compare);
+      int maxPlatforms = 1;
+      for (int i = 0; i < N; i++) {
+         int a = times[i].first;
 
-      /* SOLVE */
+         int platforms = 1;
+         for (int j = i - 1; j >= 0; j--) {
+            int b1 = times[j].first;
+            int b2 = times[j].second;
+            if (a >= b1 && a < b2)
+               platforms++;
+            else
+               break;
+         }
+         if (platforms > maxPlatforms)
+            maxPlatforms = platforms;
+      }
+
+      cout << to_string(maxPlatforms) << endl;
 
 
    }
